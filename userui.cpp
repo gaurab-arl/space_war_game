7fb53f480ad08d13098050934f60927dfb7afa20
Gaurab Aryal <gaurabaryal94@gmail.com>
2025-09-10 09:51:32 +0545

    Refactor .gitignore, update Makefile for debug flags, remove game over screen, and adjust asset paths

--- userui.cpp
+++ userui.cpp
@@ -6,7 +6,7 @@
 int x = 0, y = 0;
 
 void background_image() {
-   readimagefile("images/deep_space_background.bmp", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+   readimagefile("assets/deep_space_background.bmp", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 
 void title() {
